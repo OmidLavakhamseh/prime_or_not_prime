f10be04f062019da55af17fae76c7494e73508ef
OmidLavakhamseh <90330954+OmidLavakhamseh@users.noreply.github.com>
2023-07-19 13:36:45 +0200

    Add files via upload
    writing the program in C programming language

---
+++ test.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+   int age;
+   printf("Enter input value:");
+
+   scanf("%d",&age);//To take input from the user
+   printf("Age=%d",age);
+
+
+   
+   
+    return 0;
+}
+// double shows 6 digts after floating point
+/* */
+
